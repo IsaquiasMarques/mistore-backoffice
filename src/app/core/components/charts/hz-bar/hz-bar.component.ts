850ade1c15cc75a471106dcdf46a5438a917f76c
IsaquiasMarques <patisaquias2000@gmail.com>
2024-03-02 10:31:58 +0100

    chart: flexible width and super positioning header

--- src/app/core/components/charts/hz-bar/hz-bar.component.ts
+++ src/app/core/components/charts/hz-bar/hz-bar.component.ts
@@ -12,17 +12,56 @@ export class HzBarComponent implements OnInit, OnChanges {
       description: 'Análise de estados de produtos por venda',
       chartUnity: 'AOA'
     },
-    labels: [  "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul" ],
+    labels: [
+      "Jan",
+      "Fev",
+      // "Mar",
+      // "Abr",
+      // "Mai",
+      // "Jun",
+      // "Jul",
+      // "Ago",
+      // "Set",
+      // "Out",
+      // "Nov",
+      // "Dez"
+    ],
     series: [
       {
         name: 'Ganhos',
         color: '#61C554',
-        data: [ 10123, 13345, 35235, 25223, 64213, 23000, 50522 ]
+        data: [
+          10123,
+          13345,
+          // 35235,
+          // 25223,
+          // 64213,
+          // 23000,
+          // 50522,
+          // 23409,
+          // 21345,
+          // 53234,
+          // 34563,
+          // 54322
+        ]
       },
       {
         name: 'Perdas',
         color: "#ddd",
-        data: [ 23000, 23000, 45123, 25342, 56000, 23553, 70000 ]
+        data: [
+          13000,
+          23000,
+          // 45123,
+          // 35342,
+          // 56000,
+          // 23553,
+          // 70000,
+          // 23409,
+          // 21345,
+          // 64234,
+          // 14563,
+          // 14322
+        ]
       }
     ]
   }
