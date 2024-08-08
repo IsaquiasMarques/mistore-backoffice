6537593664c886360347e87494cf9d783f55164a
IsaquiasMarques <patisaquias2000@gmail.com>
2024-08-08 19:31:01 +0100

    completed design message page

--- src/app/feature-modules/store-admin/components/views/messages/messages.component.ts
+++ src/app/feature-modules/store-admin/components/views/messages/messages.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'mi-messages',
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit, AfterViewInit {
+  @ViewChild('messagesContainer') messagesContainer!: ElementRef<HTMLElement>;
+  @ViewChild('messageCardsContainer') messageCardsContainer!: ElementRef<HTMLElement>;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.messagesContainer.nativeElement.scrollTo(0, this.messageCardsContainer.nativeElement.clientHeight);
+  }
 
 }
