ec00a0346384b75094ebfc3c6541ccb0bbac8299
IsaquiasMarques <patisaquias2000@gmail.com>
2025-04-03 18:34:17 +0100

    building the authentication pages

--- src/app/feature-modules/auth/components/views/sign-up/sign-up.component.ts
+++ src/app/feature-modules/auth/components/views/sign-up/sign-up.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  userAgree: boolean = false;
+  
+  changeAgreementStatus(){
+    this.userAgree = !this.userAgree;
+  }
+
 }
